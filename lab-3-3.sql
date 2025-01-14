18c1129723a713cd146b57abced73e9e29bd03bc
Laura Segre <laura.segre@kellogg.northwestern.edu>
2025-01-14 21:52:24 +0000

    made changes to lab exercises

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,6 +1,9 @@
 -- In the modern era (1960-present), how many regular season games did the best team win each season?
 -- Challenge: Try to include the team's name in the results. Is it accurate?
 
+SELECT year, name, wins, MAX(wins)
+FROM teams
+
 -- Expected result: 61 rows, starting with
 --
 -- +------+-------------------------------+-----------+
